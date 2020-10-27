0bea92b91645265ee96d329630bf0811d8779dde
JustinMartin7x <justin.martin7x@gmail.com>
2020-10-27 16:55:09 -0700

    added selection

--- src/CreatureList.js
+++ src/CreatureList.js
@@ -27,11 +27,14 @@ export default class CreatureList extends Component {
             if (!this.state.filter) {
 
                 return true;
-            }
-
-            if (image.horns === Number(this.state.filter)) {
+            } else if (image.horns === Number(this.state.filter)) {
                 return true;
-            }
+            } else if (image.keyword === this.state.filter) {
+                return true;
+            } else
+                return false
+
+
         });
 
 
@@ -39,13 +42,32 @@ export default class CreatureList extends Component {
 
         return (
             <>
-                <select onChange={this.handleChange}>
-                    <option value=''>Show All</option>
-                    <option value='1'>One Horn</option>
-                    <option value='2'>Two Horns</option>
-                    <option value='3'>Three Horns</option>
-                    <option value='100'>One Hundred</option>
-                </select>
+                <section className="searches">
+                    <div className="sorter"> <div>Sort By Number of Horns</div>
+                        <select onChange={this.handleChange}>
+                            <option value=''>Show All</option>
+                            <option value='1'>One Horn</option>
+                            <option value='2'>Two Horns</option>
+                            <option value='3'>Three Horns</option>
+                            <option value='100'>One Hundred</option>
+                        </select>
+                    </div>
+                    <div className="sorter"> <div>Sort by Keyword</div>
+                        <select onChange={this.handleChange}>
+                            <option value=''>Show All</option>
+                            <option value="narwhal">Narwal</option>
+                            <option value="rhino">Rhino</option>
+                            <option value="unicorn">Unicorn</option>
+                            <option value="unilego">Unilego</option>
+                            <option value="markhor">Markhor</option>
+                            <option value="mouflon">Mouflon</option>
+                            <option value="addax">Addax</option>
+                            <option value="chameleon">Chameleon</option>
+                            <option value="lizard">Lizard</option>
+                            <option value="dragon">Dragon</option>
+                        </select>
+                    </div>
+                </section>
                 <div className="container">
                     {
                         filteredImage.map(image =>
